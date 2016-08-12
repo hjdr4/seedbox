8d2b0258311bdacafb12819962baf47b9706e677
Vincent Miszczak <vincent.miszczak@gmail.com>
2016-08-13 01:45:22 +0200

    sanitize file names to work with ACD

--- move.sh
+++ move.sh
@@ -2,6 +2,8 @@
 source="$1"
 destination="$2"
 
+cleanSource=`echo "$source"|sed 's/\[.*\]\s*//'`
+if [ "$source" != "$cleanSource" ]; then mv "$source" "$cleanSource"; source="$cleanSource";fi
 while true
 do
 	mv -f "$source" "$destination"
