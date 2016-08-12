bb5cc3792a6a1f13f5eb9b40f7cd70faf76e5c80
Vincent Miszczak <vincent.miszczak@gmail.com>
2016-08-13 00:07:55 +0200

    update

--- startDownloadSuite.sh
+++ startDownloadSuite.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /root/remount.sh
-qbittorrent-nox &
+qbittorrent-nox -d
 python /root/CouchPotatoServer/CouchPotato.py --daemon
 python /root/SickRage/SickBeard.py --nolaunch --daemon
 exec /bin/bash
